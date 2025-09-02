4e6f22d24cb7f99807285b7119d5447d73f2a1e2
Vedanti Shelar <shelarvedanti485@gmail.com>
2025-09-02 19:50:00 +0530

    Day 1 - Reverse a string

--- Solution1.java
+++ Solution1.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
-public class Solution1 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        Solution1 sol = new Solution1();
-        String result = sol.solve(input);
-        System.out.println(result);
+public class Solution1 { 
+    public static void main(String[] args) { 
+        Scanner sc = new Scanner(System.in);  
+        String input = sc.nextLine(); 
+        Solution1 sol = new Solution1(); 
+        String result = sol.solve(input); 
+        System.out.println(result); 
     }
     
     public String solve(String s) {
@@ -14,15 +14,15 @@ public class Solution1 {
             return "";
         }
         char[] chars = s.toCharArray();
-        int left = 0;
+        int left = 0; 
         int right = chars.length - 1;
-        while (left < right) {
+        while (left < right) { 
             char temp = chars[left];
             chars[left] = chars[right];
-            chars[right] = temp;
-            left++;
+            chars[right] = temp; 
+            left++; 
             right--;
-        }
+        } 
         return new String(chars);
-    }
+    } 
 }
