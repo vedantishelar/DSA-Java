3a9fe6f0820ece6d8c860f26f65026cab99af437
Vedanti Shelar <shelarvedanti485@gmail.com>
2025-09-02 19:51:56 +0530

    Day 1 - Reverse a string using recursion

--- Solution2.java
+++ Solution2.java
@@ -2,17 +2,17 @@ import java.util.Scanner;
 
 public class Solution2 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        System.out.println(reverseString(input));
+        Scanner sc = new Scanner(System.in); 
+        String input = sc.nextLine(); 
+        System.out.println(reverseString(input)); 
     }
 
-    public static String reverseString(String s) {
+    public static String reverseString(String s) { 
         if (s == null || s.length() <= 1) {
-            return s;
+            return s; 
         }
         char lastChar = s.charAt(s.length() - 1);
         String reversedSubstring = reverseString(s.substring(0, s.length() - 1));
-        return lastChar + reversedSubstring;
+        return lastChar + reversedSubstring; 
     }
 }
