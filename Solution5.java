b467b8258d416d9db4d24998ffc3d04c6e31db22
Vedanti Shelar <shelarvedanti485@gmail.com>
2025-09-03 20:32:40 +0530

    Day 3 - Count frequency of characters in a string

---
+++ Solution5.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Solution5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Solution5 sol = new Solution5();
+        
+        String input = sc.nextLine();
+        LinkedHashMap<Character, Integer> result = sol.solve(input);
+        
+        for (Map.Entry<Character, Integer> entry : result.entrySet()) {
+            System.out.println(entry.getValue());
+        }
+    }
+    public LinkedHashMap<Character, Integer> solve(String input) {
+        LinkedHashMap<Character, Integer> charCount = new LinkedHashMap<>();
+        
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (charCount.containsKey(c)) {
+                charCount.put(c, charCount.get(c) + 1);
+            } else {
+                charCount.put(c, 1);
+            }
+        }
+        
+        return charCount;
+    }
+}
