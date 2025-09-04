89385559469208c8c6410fafd97e9b879c319631
Vedanti Shelar <shelarvedanti485@gmail.com>
2025-09-04 20:24:08 +0530

    Day 4 - Check if a string contains only digits

---
+++ Solution7.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution7 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        boolean result = result(str);
+        System.out.println(result);
+    }
+    public static boolean result(String str){
+        boolean res = false;
+        if(str.matches("[0-9]+")){
+            res = true;
+        }
+        return res;
+    }
+}
