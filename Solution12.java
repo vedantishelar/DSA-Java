b0860e948c0ffc4efc002f3984d436586e29bd1e
Vedanti Shelar <shelarvedanti485@gmail.com>
2025-09-07 17:31:27 +0530

    Day 6 - Pattern printing (stars, numbers, pyramids)

--- Solution12.java
+++ Solution12.java
@@ -6,12 +6,12 @@ public class Solution12{
         int input=in.nextInt();
 
          for(int i=1; i<=input; i++) {
-             for(int j=1; j<=i; j++) {
-                 System.out.print("*");
-             }
+             for(int j=1; j<=i; j++) {  
+                 System.out.print("*"); 
+             } 
              System.out.println();
          }
-
+  
         for(int i=input; i>=1; i--) {
             for(int j=1; j<=i-1; j++) {
                 System.out.print("*");
