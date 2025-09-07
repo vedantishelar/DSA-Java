98605703334c77558537b261b0bc1e423c17daa8
Vedanti Shelar <shelarvedanti485@gmail.com>
2025-09-07 19:28:19 +0530

    Day 2 - Check if a string is a palindrome

--- Strings(13 Questions)/Solution3.java
+++ Strings(13 Questions)/Solution3.java
@@ -1,7 +1,6 @@
 // Check if a string is a palindrome
 
 import java.util.Scanner;
-
 public class Solution3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
