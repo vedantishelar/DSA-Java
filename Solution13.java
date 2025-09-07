65e6d8854b977dce188e8d5364b7424010148730
Vedanti Shelar <shelarvedanti485@gmail.com>
2025-09-07 17:49:56 +0530

    Day 6 - Toggle case of characters in a string

---
+++ Solution13.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Solution13{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        String result = "";
+
+        for(int i=0; i<str.length(); i++) {
+            char ch = str.charAt(i);
+            if(Character.isLowerCase(ch)) {
+              result = result + Character.toUpperCase(ch);
+            }
+            else if(Character.isUpperCase(ch)) {
+                result = result + Character.toLowerCase(ch);
+            }
+            else {
+               result = result + ch;
+            }
+        }
+        System.out.println(result);
+    }
+}
