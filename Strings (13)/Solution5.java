e1c27ee277a0f2b02f71cc649882e879b7344661
Vedanti Shelar <shelarvedanti485@gmail.com>
2025-09-13 23:59:38 +0530

    Day 2 - (5) Count frequency of characters in a string using hashmap

---
+++ Strings (13)/Solution5.java
@@ -0,0 +1,24 @@
+// Count frequency of characters in a string using hashmap
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Solution5 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+
+        HashMap<Character,Integer>map=new HashMap<>();
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+
+        for(char ch:map.keySet()){
+            System.out.println(ch+" "+map.get(ch));
+        }
+
+        in.close();
+        
+    }
+}
