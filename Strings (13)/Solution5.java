b7d6fd4c33f0a469205ec404e5edf51450bec4f6
Vedanti Shelar <shelarvedanti485@gmail.com>
2025-09-14 13:15:52 +0530

    Day 2 - (5) Count frequency of characters in a string using hashmap

--- Strings (13)/Solution5.java
+++ Strings (13)/Solution5.java
@@ -1,20 +1,55 @@
 // Count frequency of characters in a string using hashmap
 
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 public class Solution5 {
     public static void main(String[] args) {
+
         Scanner in = new Scanner(System.in);
         String str = in.nextLine();
 
-        HashMap<Character,Integer>map=new HashMap<>();
-        for(int i=0;i<str.length();i++){
-            char ch=str.charAt(i);
+        LinkedHashMap<Character,Integer>map = new LinkedHashMap<>();
+
+        for( int i = 0; i < str.length(); i++ ) {
+
+            char ch = str.charAt(i);
+
             map.put(ch,map.getOrDefault(ch,0)+1);
         }
 
-        for(char ch:map.keySet()){
+        for( char ch : map.keySet() ) {
+            System.out.println(map.get(ch));
+        }
+
+        in.close();
+        
+    }
+}
+
+// other solution
+
+/*
+
+// Count frequency of characters in a string using hashmap
+
+import java.util.*;
+
+public class Solution5 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+
+        HashMap<Character,Integer>map = new HashMap<>();
+
+        for( int i = 0; i < str.length(); i++ ) {
+
+            char ch = str.charAt(i);
+
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+
+        for( char ch : map.keySet() ) {
             System.out.println(ch+" "+map.get(ch));
         }
 
@@ -22,3 +57,6 @@ public class Solution5 {
         
     }
 }
+
+
+ */
