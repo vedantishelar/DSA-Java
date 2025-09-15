b407b136354d9ba7e481a089d71e7c05e5f016e5
Vedanti Shelar <shelarvedanti485@gmail.com>
2025-09-15 23:37:01 +0530

    Day 4 - (10) Print all subsets of a string using recursion

---
+++ Strings (13)/Solution10.java
@@ -0,0 +1,20 @@
+// Print all subsets of a string using recursion
+
+import java.util.*;
+
+public class Solution10 {
+    public static void main(String[] agrs) {
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        printSubset(str, 0, "");
+    }
+
+    public static void printSubset(String str, int index, String curr) {
+        if (index == str.length()) {
+            System.out.println(curr);
+            return;
+        }
+        printSubset(str, index +1, curr);
+        printSubset(str, index + 1, curr + str.charAt(index));
+    }
+}
