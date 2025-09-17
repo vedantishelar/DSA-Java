4fb59d40e6e50332f77fe7611ad2e125d3dc3c4e
Vedanti Shelar <shelarvedanti485@gmail.com>
2025-09-17 20:41:32 +0530

    Day 6 - (13 - b) Pattern printing -- Inverted-Angled Triangle of Stars

---
+++ Strings (13)/Solution13b.java
@@ -0,0 +1,17 @@
+// Pattern printing -- Inverted-Angled Triangle of Stars
+
+import java.util.Scanner;
+
+public class Solution13b {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        for (int i = n; i >= 1; i--) {   
+            for (int j = 1; j <= i; j++) {   
+                System.out.print("*");
+            }
+            System.out.println();   
+        }
+    }
+}
