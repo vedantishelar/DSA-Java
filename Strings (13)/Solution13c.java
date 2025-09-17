3f30d691b41e829b0b48151615bd306f0f52d6f0
Vedanti Shelar <shelarvedanti485@gmail.com>
2025-09-17 20:54:34 +0530

    Day 6 - (13 - c) Pattern printing -- Pyramid of Stars

---
+++ Strings (13)/Solution13c.java
@@ -0,0 +1,21 @@
+// Pattern printing -- Pyramid of Stars
+
+import java.util.*;
+public class Solution13c {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            
+            System.out.println();  
+        }
+    }
+}
