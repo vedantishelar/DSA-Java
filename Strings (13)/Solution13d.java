1e5e9f93be4b7c7732be77360810cd80bd75f0c9
Vedanti Shelar <shelarvedanti485@gmail.com>
2025-09-24 20:48:05 +0530

    Day 7 - (13 - d) Pattern printing -- Inverted Pyramid of Stars

--- Strings (13)/Solution13d.java
+++ Strings (13)/Solution13d.java
@@ -1,3 +1,22 @@
+// Pattern printing -- Inverted Pyramid of Stars
+
+import java.util.*;
+
 public class Solution13d {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); 
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            
+            for (int k = 0; k < 2 * (n - i) - 1; k++) {
+                System.out.print("*");
+            }
+            
+            System.out.println();  
+        }
+    }
 }
