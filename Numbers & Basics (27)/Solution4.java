7e6a07c3227c03e155f4a027e431d27654a5f655
Vedanti Shelar <shelarvedanti485@gmail.com>
2025-10-02 13:05:13 +0530

    Day 9 - (4) Print all primes in range

--- Numbers & Basics (27)/Solution4.java
+++ Numbers & Basics (27)/Solution4.java
@@ -1,7 +1,6 @@
 // Print all primes in range 
 
-import java.util.Scanner;
-
+import java.util.*;
 public class Solution4 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -21,12 +20,14 @@ public class Solution4 {
 
     public static boolean isPrime(int num) {
         if (num <= 1) return false; 
-        if (num == 2) return true;  
+        if (num == 2) return true; 
+
         for (int i = 2; i <= num / 2; i++) {  
             if (num % i == 0) {
                 return false;
             }
         }
+
         return true;
     }
 }
