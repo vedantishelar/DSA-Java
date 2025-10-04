2305d82777e3859d60f148cb4761120d3f9f2c3c
Vedanti Shelar <shelarvedanti485@gmail.com>
2025-10-04 20:16:07 +0530

    Day 10 - (14) Count number of 1s in binary (Hamming weight)

--- Numbers & Basics (27)/Solution14.java
+++ Numbers & Basics (27)/Solution14.java
@@ -1,6 +1,6 @@
 // Count number of 1s in binary (Hamming weight)
 
-import java.util.*;
+import java.util.*; 
 public class Solution14 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -14,7 +14,7 @@ public class Solution14 {
         int count = 0;
 
         while (n > 0) {
-            count = count + (n & 1); // Increment count if the last bit is 1
+            count = count + (n & 1);    // Increment count if the last bit is 1
             n = n >> 1;       // Right shift n to process the next bit
         }
 
